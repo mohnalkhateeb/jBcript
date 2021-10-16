21c3ae0a5b5dee4589e27fb80fdf7b3fc6b4e555
mohnalkhateeb <mhmmd.alkateeb@gmail.com>
2021-10-16 03:27:35 +0300

    first-commit

---
+++ src/main/java/com/lab14/jBcript/JBcriptApplication.java
@@ -0,0 +1,13 @@
+package com.lab14.jBcript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JBcriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JBcriptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab14/jBcript/JBcriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab14.jBcript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JBcriptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
